7f102c9ee1825ece7f0b1b2645c0181be2808558
christopher sparks <csparksnj@gmail.com>
2023-04-10 22:39:12 -0600

    added bootstrap

--- index.php
+++ index.php
@@ -13,13 +13,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $instagram = $_POST["instagram"];
 
     $unique_url = generateUniqueUrl($name);
+    $short_code = generateShortCode();
 
-    $sql = "INSERT INTO follow_me_table (name, unique_url, facebook, twitter, instagram) VALUES (?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO follow_me_table (name, short_code, unique_url, facebook, twitter, instagram) VALUES (?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sssss", $name, $unique_url, $facebook, $twitter, $instagram);
+    $stmt->bind_param("ssssss", $name, $short_code, $unique_url, $facebook, $twitter, $instagram);
 
     if ($stmt->execute()) {
-        header("Location: profile.php?id=$unique_url");
+        header("Location: profile.php?id=$short_code");
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
@@ -33,6 +34,10 @@ function generateUniqueUrl($name) {
     return md5($name . time());
 }
 
+function generateShortCode() {
+    return substr(md5(uniqid(rand(), true)), 0, 8);
+}
+
 
 ?>
 
@@ -42,24 +47,42 @@ function generateUniqueUrl($name) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Social Media App</title>
+    <!-- Add the Bootstrap CSS file -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Register</h1>
-    <form action="index.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" required><br><br>
-        
-        <label for="facebook">Facebook:</label>
-        <input type="url" name="facebook" id="facebook"><br><br>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h1 class="text-center mt-5">Social Media App</h1>
+                <form action="index.php" method="post" class="mt-4">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" name="name" id="name" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="facebook">Facebook:</label>
+                        <input type="text" name="facebook" id="facebook" class="form-control" placeholder="Facebook username">
+                    </div>
+                    <div class="form-group">
+                        <label for="twitter">Twitter:</label>
+                        <input type="text" name="twitter" id="twitter" class="form-control" placeholder="Twitter username">
+                    </div>
+                    <div class="form-group">
+                        <label for="instagram">Instagram:</label>
+                        <input type="text" name="instagram" id="instagram" class="form-control" placeholder="Instagram username">
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Generate URL</button>
+                </form>
+            </div>
+        </div>
+    </div>
 
-        <label for="twitter">Twitter:</label>
-        <input type="url" name="twitter" id="twitter"><br><br>
-
-        <label for="instagram">Instagram:</label>
-        <input type="url" name="instagram" id="instagram"><br><br>
-
-        <button type="submit">Register</button>
-    </form>
+    <!-- Add the Bootstrap and jQuery JavaScript files -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+
--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@ require_once 'db_config.php';
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 $id = $_GET["id"];
-$sql = "SELECT name, facebook, twitter, instagram FROM follow_me_table WHERE unique_url = ?";
+$sql = "SELECT name, facebook, twitter, instagram FROM follow_me_table WHERE short_code = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $id);
 $stmt->execute();
@@ -29,34 +29,66 @@ $conn->close();
 
 $full_link = "https://carcraze.co/followme/profile.php?id=" . $id;
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($name); ?>'s Profile</title>
-    <script>
-        function followAll() {
-            <?php if ($facebook): ?>window.open('<?php echo htmlspecialchars($facebook); ?>', '_blank');<?php endif; ?>
-            <?php if ($twitter): ?>window.open('<?php echo htmlspecialchars($twitter); ?>', '_blank');<?php endif; ?>
-            <?php if ($instagram): ?>window.open('<?php echo htmlspecialchars($instagram); ?>', '_blank');<?php endif; ?>
-        }
-    </script>
+    <!-- Add the Bootstrap CSS file -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1><?php echo htmlspecialchars($name); ?>'s Profile</h1>
-    <?php if ($facebook): ?><p><a href="<?php echo htmlspecialchars($facebook); ?>" target="_blank">Facebook</a></p><?php endif; ?>
-    <?php if ($twitter): ?><p><a href="<?php echo htmlspecialchars($twitter); ?>" target="_blank">Twitter</a></p><?php endif; ?>
-    <?php if ($instagram): ?><p><a href="<?php echo htmlspecialchars($instagram); ?>" target="_blank">Instagram</a></p><?php endif; ?>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h1 class="text-center mt-5"><small><a href='https://carcraze.co/followme/'>Follow Me</a></small></h1>
+                <h1 class="text-center mt-5"><?php echo htmlspecialchars($name); ?>'s Profile</h1>
+                <div class="mt-4">
+                    <h4>Social Media Profiles</h4>
+                    <ul class="list-unstyled">
+                        <?php if (!empty($facebook)): ?>
+                            <li>
+                                <strong>Facebook:</strong> <a href="<?php echo htmlspecialchars($facebook); ?>" target="_blank"><?php echo htmlspecialchars($facebook); ?></a>
+                            </li>
+                        <?php endif; ?>
+                        <?php if (!empty($twitter)): ?>
+                            <li>
+                                <strong>Twitter:</strong> <a href="<?php echo htmlspecialchars($twitter); ?>" target="_blank"><?php echo htmlspecialchars($twitter); ?></a>
+                            </li>
+                        <?php endif; ?>
+                        <?php if (!empty($instagram)): ?>
+                            <li>
+                                <strong>Instagram:</strong> <a href="<?php echo htmlspecialchars($instagram); ?>" target="_blank"><?php echo htmlspecialchars($instagram); ?></a>
+                            </li>
+                        <?php endif; ?>
+                        <li>
+                        <b>Shareable Link:</b> <span id="content"><a href="<?php echo $full_link; ?>"><?php echo $full_link; ?></a></span>
+                        <button class='mt-3 btn btn-primary btn-block' onclick="copyInnerHTML()">Copy Link</button>
+                        
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
     
 
-    Shareable Link: <span id="content"><?php echo $full_link; ?></span>
-    <button onclick="copyInnerHTML()">Copy Inner HTML</button>
+    <!-- Add the Bootstrap and jQuery JavaScript files -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+
 
     <script>
         async function copyInnerHTML() {
             var contentDiv = document.getElementById("content");
-            var innerHTML = contentDiv.innerHTML;
+            var innerHTML = contentDiv.innerText;
 
             try {
                 await navigator.clipboard.writeText(innerHTML);
@@ -67,12 +99,5 @@ $full_link = "https://carcraze.co/followme/profile.php?id=" . $id;
         }
     </script>
 
-
-    <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v16.0&appId=557244249120978&autoLogAppEvents=1" nonce="7lA0EvzT"></script>
-
-
-</body>
-</html>
 
 
